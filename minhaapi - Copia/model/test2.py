d87a32e1db51cc3fd14df40ab78215f799e9f124
rafaelmatias123 <127124514+rafaelmatias123@users.noreply.github.com>
2024-12-14 14:26:39 -0300

    Delete minhaapi - Copia directory

--- minhaapi - Copia/model/test2.py
+++
@@ -1,5 +0,0 @@
-import os
-
-db_path = os.path.abspath("database/meu_banco.sqlite3")
-db_url = f"sqlite:///{db_path}"
-print(f"URL de acesso ao banco: {db_url}")
--- minhaapi - Copia/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from schemas.encomenda import EncomendaSchema, EncomendaBuscaSchema, EncomendaViewSchema, \
-    ListagemEncomendasSchema, EncomendaDelSchema, apresenta_encomendas, \
-    apresenta_encomenda
-from schemas.error import ErrorSchema
